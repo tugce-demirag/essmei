4d8548aef3ce208b09b313b6afea05b1a010734f
Tugce Demirag <tugcedemirag@gmail.com>
2024-10-19 15:02:31 +0300

    jQuery updates
    Filters for the products and selected states of the filters menu are added to the project.

--- main.js
+++ main.js
@@ -62,4 +62,83 @@ if ($('.filters-list').is(':visible')){
     $('.show-filters').text('Hide filters')
 }
 
+// Return false because Show filters link href=# is making the website jump when we click smt
+// return false bloks the default action
+    return false
+
 })
+
+
+
+// trying if the data attribute works here data-filters
+// it's weird that without return false  - the console in the chrome dev tools
+// didn't display "it works!" but after I wrote return false
+// which is for blocking the href to go to the page in any link for example all.html
+// return false blocks the default action, this behaviour and it also enables console 
+// to print "it works"
+
+
+//$('.filters-list a').on('click', function(){
+//console.log('it works!')
+//return false
+//})
+
+
+// instead of "it works!" we write this 
+// which gives us entire <a> line with all the details
+// when we click bananas link on the website chrome dev console
+// prints <a href="bandanas.html" data-filter=".bandanas">Bandanas</a>
+
+//$('.filters-list a').on('click', function(){
+
+//   console.log(this)
+//    return false
+    
+//    })
+
+// next step would be, selecting the data-filter in each and every a tag
+// with some modification
+// now console only prints .bags .bandanas - basically what we had written
+// to ...data-filters =".bandanas"> in html
+
+
+//$('.filters-list a').on('click', function(){
+
+//    console.log($(this).data('filter'))
+//    return false
+    
+//    })
+
+// exact same thing as above but smarter 
+// because we paste the value to a variable and then print it to the console
+
+$('.filters-list a').on('click', function(){
+
+    var filter = $(this).attr('data-filter')
+
+    console.log(filter)
+
+    // 1- hide all the products
+    // 2- show the products with a particular filter 
+    // when we say $('.bags).show() - it will show all the products with bags class name
+    // but we will use our variable filter
+
+    $('.product').hide()
+    $(filter).show()
+
+    // we want to grab the clicked link- The way we do it is $(this)
+    // below adds a selected class to the filter link we just clicked on
+    // but this makes every filter link on selected state not removing the
+    // selected state from the passive links- we need to toggle off the others
+    // so just before we add a class name called 'selected' we are going to grab
+    // all the '.filters-list a' and remove the class name called 'selected'
+
+   // $(this).addClass('selected')
+
+    $('.filters-list a').removeClass('selected')
+    $(this).addClass('selected')
+
+
+    return false
+    
+    })
